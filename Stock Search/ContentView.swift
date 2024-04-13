4b92e1562bd5f09c6175656ef92adbd5b95625ff
Dharanidhar Reddy <yerramdharanidhar@gmail.com>
2024-04-13 08:37:06 -0700

    Initial Commit

---
+++ Stock Search/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stock Search
+//
+//  Created by Babloo Yerram on 4/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stock Search/Stock_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_SearchApp.swift
+//  Stock Search
+//
+//  Created by Babloo Yerram on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Stock_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
